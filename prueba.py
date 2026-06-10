71178e4c1a4b3468be57e9b38d870faf5164bded
jalanza2002 <93798423+jalanza2002@users.noreply.github.com>
2026-06-10 15:08:20 -0600

    delete files that I never use

--- prueba.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-df=pd.read_csv("results.csv", sep=",")
-costa_rica_tournaments=df[
-    (df["home_team"]=="Costa Rica") |
-    (df["away_team"]=="Costa Rica")
-]
-
-print(costa_rica_tournaments["tournament"].unique())
